6544487b0aea42dd092b9cfbb50cc4603d6a7e74
BrendanC <>
2024-09-27 13:14:08 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/versioncontrol_brendancoleman/VersionControl_BrendanColeman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_brendancoleman;
+
+/**
+ *
+ * @author blazi
+ */
+public class VersionControl_BrendanColeman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
